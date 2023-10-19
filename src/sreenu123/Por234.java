d100cbf852b529f2539487fbbcb4c4743c7d8773
lenovo <lenovo@DESKTOP-K25VCFG>
2023-10-19 10:58:54 +0530

    Commit the project

--- src/sreenu123/Por234.java
+++ src/sreenu123/Por234.java
@@ -3,8 +3,8 @@ package sreenu123;
 public class Por234 {
 
 	public static void main(String[] args) {
-	System.out.println("SREENU");
-
+		System.out.println("SREENU");
+		System.out.println("Sivakumar");
 	}
 
 }
